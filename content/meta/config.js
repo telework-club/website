04bd9b544ac76b0cd4729655f8770f34506174d6
Lu Rongkai <lurongkai@gmail.com>
2020-04-09 10:23:52 +0800

    cleanup

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,39 +1,32 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
-  // pathPrefix: "",
+  siteTitle: "Telework Community", // <title>
+  shortSiteTitle: "Telework", // <title> ending for posts and pages
+  siteDescription: "Telework Community",
+  siteUrl: "https://telework.club",
+  pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Telework Community",
+  authorTwitterAccount: "telework-club",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "远程工作社区",
+  headerSubTitle: "定义新的工作方式",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "Telework Community",
+  manifestShortName: "Telework Community", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
   manifestDisplay: "standalone",
 
-  // gravatar
-  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
-  // Replace your email adress with md5-code.
-  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
-  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
-  gravatarImgMd5: "",
-
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" },
+    { name: "github", url: "https://github.com/telework-club" },
+    { name: "twitter", url: "https://twitter.com/telework-club" },
+    { name: "facebook", url: "http://facebook.com/telework-club" },
   ],
 };
